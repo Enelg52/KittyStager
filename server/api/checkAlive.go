ae2fce21fae562e7cde0ea1735e97d703dbcd526
Enelg52 <yann.grossenbacher@bluewin.ch>
2023-03-20 11:48:18 +0100

    fix checkAlive

--- server/api/checkAlive.go
+++ server/api/checkAlive.go
@@ -9,13 +9,13 @@ func checkAlive(name string) {
 	for {
 		k := Kittens[name]
 		time.Sleep(1 * time.Second)
-		if k.GetAlive() {
-			t := time.Since(k.GetLastSeen())
-			sleepTime := time.Duration(k.GetSleep()) * time.Second
-			if t > sleepTime+5*time.Second {
-				k.SetAlive(false)
-			}
+		t := time.Since(k.GetLastSeen())
+		sleepTime := time.Duration(k.GetSleep()) * time.Second
+		if t > sleepTime+5*time.Second {
+			k.SetAlive(false)
 		}
-		return
+		if !k.GetAlive() {
+			return
+		}
 	}
 }
